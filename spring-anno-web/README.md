5f34df3e71a432f130b44e30fa9e8e7ddf88837b
Ranjith Sekar <java2ranjith@gmail.com>
2020-02-29 17:03:20 +0530

    up

--- spring-anno-web/README.md
+++ spring-anno-web/README.md
@@ -1,9 +1,12 @@
 ### TOOLS & TECHNOLOGIES
   1. Eclipse/STS
-  2. Spring Framework
+  2. Spring Framework 
 
 ### CONCEPTS/TOPICS COVERED
-  1. @ Annotation
+  1. @EnableWebMvc Annotation
+  2. WebMvcConfigurationSupport Class
+  3. AbstractAnnotationConfigDispatcherServletInitializer Class
 
 ### HOW TO RUN?
-  1. 
+  1. Run on Tomcat Server http://localhost:8080/spring-anno-web/
+  
--- spring-anno-web/src/main/java/jbr/spring/anno/controller/AnnoController.java
+++ spring-anno-web/src/main/java/jbr/spring/anno/controller/AnnoController.java
@@ -8,12 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import jbr.spring.anno.model.Person;
 
 @Controller
-@RequestMapping
 public class AnnoController {
 
   @Autowired
